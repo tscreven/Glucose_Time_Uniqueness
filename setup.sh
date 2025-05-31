ae024ff7d98f80c62a0c6b398e574c51e4aaabb9
tscreven <tscreven@gmail.com>
2025-05-30 22:47:21 -0700

    Fixed comment in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# On commandline, enter: chmod +x script.sh. Then enter: ./setup.sh. Only need
+# On commandline, enter: chmod +x setup.sh. Then enter: ./setup.sh. Only need
 # to run this file once. 
 mkdir Data
 mkdir Data/ClarityCSV
